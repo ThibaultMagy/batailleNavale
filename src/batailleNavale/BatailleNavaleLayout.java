f2c69c7d5503a83a231087ac91d66683b52d80df
Ali Essk <aliessk@gmail.com>
2018-05-02 22:26:26 +0200

    useless

--- src/batailleNavale/BatailleNavaleLayout.java
+++ src/batailleNavale/BatailleNavaleLayout.java
@@ -35,39 +35,31 @@ public class BatailleNavaleLayout extends JPanel {
 	private JLabel jlNomJ2;
 	private TerrainPhysique OwnFieldJ1 = new TerrainPhysique();
 	private TerrainPhysique OwnFieldJ2 = new TerrainPhysique();
-<<<<<<< HEAD
-	private JLabel jlChoixPosJ = new JLabel(", placez vos bateaux sur la grille");
+
 	//private SetUpPanel j2su = new SetUpPanel();
-=======
+
 	private JLabel jlChoixPosJ = new JLabel(jlNomJ1 + ", placez vos bateaux sur la grille");
 	private JPanel glAffichageBateauxJ1 = new JPanel();
 	private JPanel glAffichageBateauxJ2 = new JPanel();
 	
 	// private SetUpPanel j2su = new SetUpPanel();
->>>>>>> refs/remotes/origin/master
+
 	private JButton jbNext = new JButton("Next");
-<<<<<<< HEAD
+
 	private JLabel erreurJ1 = new JLabel(OwnFieldJ1.getErreur());
 	private JLabel erreurJ2 = new JLabel(OwnFieldJ2.getErreur());
-	
-	
-	
-	
-=======
->>>>>>> refs/remotes/origin/master
+
 
 	BorderLayout bl = new BorderLayout();
-<<<<<<< HEAD
-			
-		public BatailleNavaleLayout(BatailleNavaleWindow bnw) {
+
+		
+
+	public BatailleNavaleLayout(BatailleNavaleWindow bnw) {
 		JPanel Error = new JPanel();
 		Error.setLayout(new BoxLayout(Error,BoxLayout.X_AXIS));
 		Error.add(erreurJ1);
 		Error.add(erreurJ2);
 		erreurJ2.setVisible(false);
-=======
-
-	public BatailleNavaleLayout(BatailleNavaleWindow bnw) {
 		jlNomJ1 = new JLabel(mpl.getNomJoueur1());
 		jlNomJ2 = new JLabel(mpl.getNomJoueur2());
 		
@@ -129,7 +121,6 @@ public class BatailleNavaleLayout extends JPanel {
 		glAffichageBateauxJ1.add(nbTorpilleur);
 		
 		
->>>>>>> refs/remotes/origin/master
 		JPanel Grid = new JPanel();
 		Grid.setLayout(new BoxLayout(Grid, BoxLayout.X_AXIS));
 		Grid.add(OwnFieldJ1);
@@ -140,29 +131,22 @@ public class BatailleNavaleLayout extends JPanel {
 		jlChoixPosJ.setHorizontalAlignment(SwingConstants.CENTER);
 		this.setLayout(bl);
 		System.out.println("Premier panel");
-<<<<<<< HEAD
-		
-
 
 		
-
-		//TEST
-		SousMarin m = new SousMarin();
-=======
 
 		// TEST
 		SousMarin m = new SousMarin();
->>>>>>> refs/remotes/origin/master
+
 		m.rotation();
-<<<<<<< HEAD
+
 		OwnFieldJ1.placerBateau(m, 9, 1);
 
 		
-=======
+
 		OwnFieldJ1.placerBateau(m, 1, 1);
 		OwnFieldJ1.placerBateau(m, 2, 1);
 
->>>>>>> refs/remotes/origin/master
+
 		jbNext.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -170,23 +154,20 @@ public class BatailleNavaleLayout extends JPanel {
 				OwnFieldJ2.setVisible(true);
 				jlChoixPosJ.setText(jlNomJ2 + ", placez vos bateaux sur la grille");
 				System.out.println("Nouveau Panel");
-<<<<<<< HEAD
+
 				erreurJ1.setVisible(false);
 				erreurJ2.setVisible(true);
 				
-=======
-
->>>>>>> refs/remotes/origin/master
 			}
 		});
-<<<<<<< HEAD
+
 		//Positionnement des elements dans le BorderLayout
 		//this.add(j1su, BorderLayout.CENTER);
 		this.add(Error, BorderLayout.CENTER);
-=======
+
 		// Positionnement des elements dans le BorderLayout
 		// this.add(j1su, BorderLayout.CENTER);
->>>>>>> refs/remotes/origin/master
+
 		this.add(jlChoixPosJ, BorderLayout.NORTH);
 		this.add(glAffichageBateauxJ1, BorderLayout.CENTER);
 		this.add(Grid, BorderLayout.EAST);
--- src/batailleNavale/TerrainPhysique.java
+++ src/batailleNavale/TerrainPhysique.java
@@ -5,27 +5,31 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JLabel;
-
 public class TerrainPhysique extends Terrain {
     // ATTRIBUTS
     private Bouton[][] bouton;
-    private String erreur = new String("test");
-
-<<<<<<< HEAD
+	private String erreur;
+    //
     // CONSTRUCTEURS
     public TerrainPhysique(int taille) {
         super(taille);
         bouton = new Bouton[taille][taille];
         this.setLayout(new GridLayout(taille, taille));
-        setTerrain(taille);
+        setTerrain(taille, taille);
+    }
+    
+    public TerrainPhysique(int taille1, int taille2) {
+        super(taille1);
+        bouton = new Bouton[taille1][taille2];
+        this.setLayout(new GridLayout(taille1,taille2));
+        setTerrain(taille1, taille2);
     }
 
     public TerrainPhysique() {
         this.setPreferredSize(new Dimension(500, 500));
         bouton = new Bouton[TAILLE][TAILLE];
         this.setLayout(new GridLayout(TAILLE, TAILLE));
-        setTerrain(TAILLE);
+        setTerrain(TAILLE, TAILLE);
     }
 
     // METHODES
@@ -44,9 +48,9 @@ public class TerrainPhysique extends Terrain {
     }
 
     // Set un terrain
-    private void setTerrain(int taille) {
-        for (int i = 0; i < taille; i++) {
-            for (int j = 0; j < taille; j++) {
+    private void setTerrain(int taille1, int taille2) {
+        for (int i = 0; i < taille1; i++) {
+            for (int j = 0; j < taille2; j++) {
                 bouton[i][j] = new Bouton();
                 if (i == 0 || j == 0) {
                     if (i == 0 && j > 0) {
@@ -73,7 +77,6 @@ public class TerrainPhysique extends Terrain {
                 if (this.getTerrainEtat(i, j) == 1) {
                     bool = false;
                     System.out.println("Impossible de placer un bateau ici, un autre entre en conflit avec !");
-                    erreur="Impossible de placer un bateau ici, un autre entre en conflit avec !";
                 }
             }
             if (bool) {
@@ -89,7 +92,6 @@ public class TerrainPhysique extends Terrain {
                     if (this.getTerrainEtat(i, j) == 1) {
                         bool = false;
                         System.out.println("Impossible de placer un bateau ici, un autre entre en conflit avec !");
-                        erreur="Impossible de placer un bateau ici, un autre entre en conflit avec !";
                     }
                 }
             }
@@ -99,7 +101,6 @@ public class TerrainPhysique extends Terrain {
                 if (this.getTerrainEtat(i, j) == 1) {
                     bool = false;
                     System.out.println("Impossible de placer un bateau ici, un autre entre en conflit avec !");
-                    erreur="Impossible de placer un bateau ici, un autre entre en conflit avec !";
                 }
                 if (bool) {
 
@@ -111,7 +112,6 @@ public class TerrainPhysique extends Terrain {
                     }
                 } else {
                     System.out.println("code a remplir");
-                    erreur="code a remplir";
                 }
 
             }
@@ -119,118 +119,8 @@ public class TerrainPhysique extends Terrain {
     }
 
 	public String getErreur() {
+		
+		
 		return erreur;
-=======
-	// CONSTRUCTEURS
-	public TerrainPhysique(int taille) {
-		super(taille);
-		bouton = new Bouton[taille][taille];
-		this.setLayout(new GridLayout(taille, taille));
-		setTerrain(taille, taille);
-	}
-	
-	public TerrainPhysique(int taille1, int taille2) {
-		super(taille1);
-		bouton = new Bouton[taille1][taille2];
-		this.setLayout(new GridLayout(taille1,taille2));
-		setTerrain(taille1, taille2);
->>>>>>> refs/remotes/origin/master
-	}
-<<<<<<< HEAD
-}
-=======
-
-	public TerrainPhysique() {
-		this.setPreferredSize(new Dimension(500, 500));
-		bouton = new Bouton[TAILLE][TAILLE];
-		this.setLayout(new GridLayout(TAILLE, TAILLE));
-		setTerrain(TAILLE, TAILLE);
-	}
-
-	// METHODES
-	// Methode permettant de vérouiller les boutons du terrain
-	public void verBoutAll() {
-		for (int i = 0; i < getTaille(); i++) {
-			for (int j = 0; j < getTaille(); j++) {
-				bouton[i][j].setEnabled(false);
-			}
-		}
-	}
-
-	// Methode pour verouiller un bouton
-	public void verBout(Bouton b) {
-		b.setEnabled(false);
-	}
-
-	// Set un terrain
-	private void setTerrain(int taille1, int taille2) {
-		for (int i = 0; i < taille1; i++) {
-			for (int j = 0; j < taille2; j++) {
-				bouton[i][j] = new Bouton();
-				if (i == 0 || j == 0) {
-					if (i == 0 && j > 0) {
-						bouton[i][j].setText(Character.toString((char) ('a' + (j - 1))));
-					} else if (i > 0 && j == 0) {
-						bouton[i][j].setText(String.valueOf((i)));
-					}
-					verBout(bouton[i][j]);
-				}
-				this.add(bouton[i][j]);
-			}
-		}
-	}
-
-	// Methode permettant la mise en place d'un bateau
-	public void placerBateau(Bateau b, int i, int j) {
-		if (!b.isTurned() && j + b.getX() > this.getTaille()) {
-			System.out.println("Impossible de placer le bateau ici !");
-		} else if (b.isTurned() && i + b.getX() > this.getTaille()) {
-			System.out.println("Impossible de placer le bateau ici !");
-		} else if (!b.isTurned()) {
-			boolean bool = true;
-			for (int y = j; y < j + b.getX() && bool; y++) {
-				if (this.getTerrainEtat(i, j) == 1) {
-					bool = false;
-					System.out.println("Impossible de placer un bateau ici, un autre entre en conflit avec !");
-				}
-			}
-			if (bool) {
-				for (int y = j; y < j + b.getX(); y++) {
-					this.setTerrainEtat(1, i, y);
-					bouton[i][y].changeColor(1);
-					verBout(bouton[i][y]);
-					System.out.println(i + "," + y);
-				}
-			} else {
-				bool = true;
-				for (int x = i; i < i + b.getX() && bool; i++) {
-					if (this.getTerrainEtat(i, j) == 1) {
-						bool = false;
-						System.out.println("Impossible de placer un bateau ici, un autre entre en conflit avec !");
-					}
-				}
-			}
-		} else {
-			boolean bool = true;
-			for (int y = j; y < j + b.getX() && bool; y++) {
-				if (this.getTerrainEtat(i, j) == 1) {
-					bool = false;
-					System.out.println("Impossible de placer un bateau ici, un autre entre en conflit avec !");
-				}
-				if (bool) {
-
-					for (int x = i; x < i + b.getX(); x++) {
-						this.setTerrainEtat(1, x, j);
-						bouton[x][j].changeColor(1);
-						verBout(bouton[x][j]);
-						System.out.println(x + "," + j);
-					}
-				} else {
-					System.out.println("code a remplir");
-				}
-
-			}
-		}
 	}
 }
->>>>>>> refs/remotes/origin/master
